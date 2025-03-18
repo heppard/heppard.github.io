5549040d5829bbbe2c56a40adffed25baac3526a
heppard <kcplummer5@gmail.com>
2025-03-18 02:55:34 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+### 6. scripts.js (JavaScript File)
+```javascript
+// Add any JavaScript interactivity here
+
+// Example: Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+```
